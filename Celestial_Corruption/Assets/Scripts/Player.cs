d239e5b8bc00fde01ec5efe990917e6a230a2e80
MartinOravecSvK <oravec.martin01@gmail.com>
2024-01-31 14:19:04 +0000

    Changed Update to FixedUpdate in the player script -> Fix for the camera

--- Celestial_Corruption/Assets/Scripts/Player.cs
+++ Celestial_Corruption/Assets/Scripts/Player.cs
@@ -25,7 +25,7 @@ public class Player : MonoBehaviour
         movementSpeed = runningSpeed;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         ToggleWalk();
         Move();
