bf7e7f386b4306f3c89a07b596c3ff19a70a14e6
IrekSwie <ok21875@bristol.ac.uk>
2024-02-03 19:33:30 +0000

    Fixed grounded raycasting, added onLand function that activates when player lands. Player 'stands up' after landing now.

--- Celestial_Corruption/Assets/Scripts/Gliding.cs
+++ Celestial_Corruption/Assets/Scripts/Gliding.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Gliding : MonoBehaviour
@@ -32,13 +33,18 @@ public class Gliding : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
     private void FixedUpdate()
-    {
+    {   
         checkGroundStatus();
         if (!isGrounded) {
             GlidingMovement();
             ManageRotation();
         };
-        Debug.Log(isGrounded);
+        if (isGrounded)
+        {
+            //Debug.Log("Grounded");
+            //transform.rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
+
+        }
     }
     // Update is called once per frame
     private void Update()
@@ -46,6 +52,12 @@ public class Gliding : MonoBehaviour
         
     }
 
+    //This function activates when isGrounded goes from False to True
+    private void onLand()
+    {
+        //Makes player stand up
+        transform.rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
+    }
     //Function used to check if the player is grounded
     private void checkGroundStatus()
     {
@@ -53,40 +65,50 @@ public class Gliding : MonoBehaviour
         //Debug.Log(groundedCheckDistance);
         RaycastHit hit;
         Vector3 rayStart = transform.position;
-        Vector3 rayDirection = -transform.up;
+        Vector3 rayDirection = Vector3.down;
         //out hit means we store the information in hit, and groundCheckDistance is how far the raycast goes
         if (Physics.Raycast(rayStart, rayDirection, out hit, groundedCheckDistance, groundLayer))
         {
+            //Checks if the player has "landed"
+            if (!isGrounded)
+            {
+                onLand();
+            }
             isGrounded = true;
         }
         else
         {
             isGrounded = false;
         }
-        Debug.DrawRay(rayStart, rayDirection * groundedCheckDistance, isGrounded ? Color.green : Color.red);
+        //Debug.DrawRay(rayStart, rayDirection * groundedCheckDistance, isGrounded ? Color.green : Color.red);
     }
     //Calculates the forces that get applied during gliding
     private void GlidingMovement()
     {
         float pitchInRads = transform.eulerAngles.x * Mathf.Deg2Rad;
+        //Pitch gets mapped to a value between -1 and 1 using Sin and multiplied by the thrust factor.
+        //If the "nose" of the character points up then it slows down, if it points down then it speeds up.
         float mappedPitch=Mathf.Sin(pitchInRads) *ThrustFactor;
         Vector3 glidingForce = Vector3.forward * CurrentThrustSpeed;
-
+        //Time.deltaTime is used to account for changes in framerate/frametime
         CurrentThrustSpeed += mappedPitch * Time.deltaTime;
+        //Forces the CurrentThrustSpeed to be between 0 and MaxThrustSpeed
         CurrentThrustSpeed = Mathf.Clamp(CurrentThrustSpeed, 0, MaxThrustSpeed);
+        //Checks if the speed of the player is high enough for gliding forces to be applied
         if (rigidBody.velocity.magnitude >= MinThrustSpeed)
         {
+            //Applies glidingForce to the direction the player is pointing
             rigidBody.AddRelativeForce(glidingForce);
         }
         else
         {
+            //If the player is below the threshold speed then the CurrentThrustSpeed is reset to 0
             CurrentThrustSpeed = 0;
         }
     }
     //Uses camera rotation to dictate character rotation and direction for forces to be applied
     private void ManageRotation()
     {
-
         Quaternion targetRotation = Quaternion.Euler(cameraTransform.eulerAngles.x, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
         transform.rotation = targetRotation;
     }
