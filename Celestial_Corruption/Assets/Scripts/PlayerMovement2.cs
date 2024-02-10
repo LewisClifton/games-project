bc233b17e808dffe20e0d1f37b9883dcc3146642
Irek Swierczynski <ok21875@bristol.ac.uk>
2024-02-10 18:08:55 +0000

    Added jumping and ground checking using raycasting into PlayerMovement2

--- Celestial_Corruption/Assets/Scripts/PlayerMovement2.cs
+++ Celestial_Corruption/Assets/Scripts/PlayerMovement2.cs
@@ -15,18 +15,25 @@ public class PlayerMovement2 : MonoBehaviour
     public Transform orientation;
     [SerializeField] private float jumpForce = 5f;
     Rigidbody playerBody;
+    Vector2 moveInput;
 
     [Header("Player State")]
     public bool isGrounded = false;
 
+    [Header("Player State Checking")]
+    private float groundedCheckDistance = 1.5f;
+    private float colliderHeight;
+    private float bufferCheckDistance = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Collider playerCollider;
 
-    Vector2 moveInput;
 
 
     void Start()
     {
         playerBody = GetComponent<Rigidbody>();
         playerBody.freezeRotation = true;
+        colliderHeight = playerCollider.bounds.size.y;
     }
 
     
@@ -35,6 +42,12 @@ public class PlayerMovement2 : MonoBehaviour
         MovePlayer();
         Jump();
     }
+
+    private void FixedUpdate()
+    {
+        checkGroundStatus();
+    }
+
 
     private void MovePlayer() { 
         Vector3 playerVelocity = orientation.forward * moveInput.y * moveSpeed + orientation.right * moveInput.x * moveSpeed;
@@ -48,11 +61,50 @@ public class PlayerMovement2 : MonoBehaviour
         moveInput=value.Get<Vector2>();
     }
 
+    //Ran every frame. Checks if player begins a jump.
     private void Jump()
     {
         if (gameInput.IsJumpPressed() && isGrounded)
         {
             playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    //Function ran everytime the player lands on the ground.
+    private void onLand()
+    {
+        // Makes player stand up
+        //transform.rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, cameraTransform.eulerAngles.z);
+        //isGliding = false;
+
+        // Reset the drag to 0
+        // playerBody.drag = 0;
+        // Reset the movement speed to running speed
+        //movementSpeed = runningSpeed;
+    }
+
+
+    //Ran every FixedUpdate, checks if the player is on the ground.
+    private void checkGroundStatus()
+    {
+        groundedCheckDistance = (colliderHeight / 2) + bufferCheckDistance;
+
+        Vector3 rayStart = transform.position;
+        Vector3 rayDirection = Vector3.down;
+
+        RaycastHit hit;
+        if (Physics.Raycast(rayStart, rayDirection, out hit, groundedCheckDistance, groundLayer))
+        {
+            //Checks if the player has "landed"
+            if (!isGrounded)
+            {
+                onLand();
+            }
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }
