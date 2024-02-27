07d74415deb69cc8aeb54d3ceff7a15793bfb8c3
MartinOravecSvK <oravec.martin01@gmail.com>
2024-02-27 23:47:12 +0000

    added timer coroutine for freezing (in case we want to use it somewhere else or differently)

--- Celestial_Corruption/Assets/Scripts/FragmentOrb/FragmentOrb.cs
+++ Celestial_Corruption/Assets/Scripts/FragmentOrb/FragmentOrb.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FragmentOrb : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private bool isFrozen = false;
 
     private void Start()
     {
@@ -20,8 +21,18 @@ public class FragmentOrb : MonoBehaviour
             // Make the player immovable
             if (playerRb != null)
             {
-                playerRb.constraints = RigidbodyConstraints.FreezeAll;
+                // playerRb.constraints = RigidbodyConstraints.FreezeAll;
+                StartCoroutine(freezeCoroutine(3f));
             }
         }
+    }
+
+    IEnumerator freezeCoroutine(float seconds)
+    {
+        isFrozen = true;
+        playerRb.constraints = RigidbodyConstraints.FreezeAll;
+        yield return new WaitForSeconds(seconds);
+        playerRb.constraints = RigidbodyConstraints.None;
+        isFrozen = false;
     }
 }
