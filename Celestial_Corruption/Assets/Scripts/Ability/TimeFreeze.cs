205c56f670119c388f727473b4696641b5c83f60
Ethan9030 <ey20693@bristol.ac.uk>
2024-02-28 18:29:00 +0000

    Adding a script can stop the object
    It can save the previous speed and direction for where the object continues to move after recovery.

--- Celestial_Corruption/Assets/Scripts/Ability/TimeFreeze.cs
+++ Celestial_Corruption/Assets/Scripts/Ability/TimeFreeze.cs
@@ -5,16 +5,14 @@ using UnityEngine.Audio;
 
 public class TimeFreeze : MonoBehaviour
 {
-    [SerializeField] float TimeScale = 0.1f;
+    [SerializeField] float TimeScale = 0.05f;
     float normalTimeScale = 1f; // the normal time scale
-    float fixedTime = 0f;
-    float maxfixedTime = 0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        fixedTime = Time.fixedDeltaTime;
-        maxfixedTime = Time.fixedDeltaTime;
+        
     }
 
     // Update is called once per frame
@@ -22,12 +20,12 @@ public class TimeFreeze : MonoBehaviour
     {
         if (Input.GetMouseButton(1))
         {
-            // Press and hold the "I" key to make time flow slower
+            // Right click to make time flow slower
             Time.timeScale = TimeScale;
         }
         else
         {
-            // Release the "I" key to resume normal time flow rate
+            // Release the mouse to resume normal time flow rate
             Time.timeScale = normalTimeScale;
         }
     }
--- Celestial_Corruption/Assets/Scripts/Enemy/Player.cs
+++ Celestial_Corruption/Assets/Scripts/Enemy/Player.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private TimeManager timemanager;
+    void Start()
+    {
+        timemanager = FindObjectOfType<TimeManager>();
+
+    }
     private void Update()
     {
+
         Vector2 inputVector = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W))
@@ -24,6 +31,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             inputVector.x = +1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.J)) //Stop Time when Q is pressed
+        {
+            timemanager.StopTime();
+            //Grayscale.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.K) && timemanager.TimeIsStopped)  //Continue Time when E is pressed
+        {
+            timemanager.ContinueTime();
+            //Grayscale.enabled = false;
         }
 
         inputVector = inputVector.normalized;
