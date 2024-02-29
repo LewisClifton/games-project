9ee467e600eafd3dcff5d8b4bd2a3ee9bec0fcff
MartinOravecSvK <oravec.martin01@gmail.com>
2024-02-29 20:14:44 +0000

    update

--- Celestial_Corruption/Assets/Scripts/FragmentOrb/FragmentOrb.cs
+++ Celestial_Corruption/Assets/Scripts/FragmentOrb/FragmentOrb.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class FragmentOrb : MonoBehaviour
 {
+    [SerializeField] private Renderer orbRenderer;
     private Rigidbody playerRb;
+    private Collider playerCollider;
     private bool isFrozen = false;
 
     private void Start()
     {
         // Assuming the player has a Rigidbody component
         playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        // Check if the collision is with the player
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            // Make the player immovable
-            if (playerRb != null && !isFrozen)
-            {
-                Debug.Log("Player collided with the orb");
-                StartCoroutine(freezeCoroutine(3f));
-                Debug.Log("Player is frozen for 3 seconds");
-            }
+            Debug.Log("Player collided with the orb");
+            orbRenderer.material.SetVector("_EnteringObjectPosition", playerCollider.size + playerCollider.center);
+            orbRenderer.material.SetFloat("_ColliderCenter", playerCollider.center);
+            orbRenderer.material.SetFloat("_ColliderSize", playerCollider.size);
+            // orbRenderer.material.SetVector("_EnteringObjectPosition", other.bounds.center);
         }
     }
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     // Check if the collision is with the player
+    //     if (collision.gameObject.CompareTag("Player"))
+    //     {
+    //         // Make the player immovable
+    //         if (playerRb != null && !isFrozen)
+    //         {
+    //             Debug.Log("Player collided with the orb");
+    //             StartCoroutine(freezeCoroutine(3f));
+    //             Debug.Log("Player is frozen for 3 seconds");
+    //         }
+    //     }
+    // }
 
     IEnumerator freezeCoroutine(float seconds)
     {
