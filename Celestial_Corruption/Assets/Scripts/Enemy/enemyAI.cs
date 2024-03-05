0cb1e2ec5e7fb8645863b969f048b297d8dc7e56
Ethan9030 <ey20693@bristol.ac.uk>
2024-03-05 01:34:12 +0000

    Fix the problem that the object fall from air will bounce back

--- Celestial_Corruption/Assets/Scripts/Enemy/enemyAI.cs
+++ Celestial_Corruption/Assets/Scripts/Enemy/enemyAI.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class enemyAI : MonoBehaviour
 {
     [SerializeField] Transform Player;
     [SerializeField] float enemySpeed;
     [SerializeField] float distance;
+    [SerializeField] float driftMin;
+    [SerializeField] float driftMax;
     private Rigidbody rb;
-    Vector3 StartPos;
+    private Vector3 StartPos;
 
-    // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("Player").transform;
@@ -19,11 +19,10 @@ public class enemyAI : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         distance = Vector3.Distance(transform.position, Player.position);
-        if (distance <= 8f ) 
+        if (distance <= 8f)
         {
             // chase
             chase();
@@ -34,38 +33,56 @@ public class enemyAI : MonoBehaviour
             goHome();
         }
     }
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            rb.isKinematic = true; 
+            rb.isKinematic = true;
+        }
+        else if (collision.gameObject.CompareTag("Ground") && rb.velocity.y < 0)
+        {
+            // When it hit the ground it sets velocity to zero
+            rb.velocity = Vector3.zero;
         }
     }
 
     void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            rb.isKinematic = false; 
+            rb.isKinematic = false;
         }
     }
 
-
     void chase()
     {
-            Vector3 direction = (Player.position - transform.position).normalized;
-            rb.MovePosition(transform.position + direction * enemySpeed * Time.deltaTime);
-            // Only target the player's x and z position
-            Vector3 targetPostition = new Vector3(Player.position.x, this.transform.position.y, Player.position.z);
-            transform.LookAt(targetPostition);
-
+        Vector3 direction = (Player.position - transform.position).normalized;
+        rb.MovePosition(transform.position + direction * enemySpeed * Time.deltaTime);
+        Vector3 targetPostition = new Vector3(Player.position.x, transform.position.y, Player.position.z);
+        transform.LookAt(targetPostition);
     }
 
     void goHome()
     {
-        Vector3 ReturnToStart = new Vector3(StartPos.x, this.transform.position.y,StartPos.z);
-        transform.LookAt(ReturnToStart); 
-        transform.position += (StartPos - transform.position) * Time.deltaTime * enemySpeed; 
+        Vector3 ReturnToStart = new Vector3(StartPos.x, transform.position.y, StartPos.z);
+        transform.LookAt(ReturnToStart);
+        transform.position += (ReturnToStart - transform.position) * Time.deltaTime * enemySpeed;
     }
 
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && rb.velocity.y < 0)
+        {
+            // Set thr draftspeed between driftMin and driftMax
+            float driftSpeed = Random.Range(driftMin, driftMax);
+
+            // Caculate the direction of reflection
+            Vector3 direction = Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal);
+
+            // Set velocity and avoid it's smaller than 0
+            rb.velocity = direction * Mathf.Max(driftSpeed, 0f);
+        }
+    }
 }
+
