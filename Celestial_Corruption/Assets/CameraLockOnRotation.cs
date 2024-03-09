c7b9193376d3bdcdc0ff1a3dff6f32ac39f63532
IrekSwie <ok21875@bristol.ac.uk>
2024-03-09 13:17:47 +0000

    Deleted unused old scenes. Added script that can 'lock on' to an enemy, very basic implementation, have to add controls for enabling it, code for enabling it and disabling it, code for selecting target, and possibly code for switching cameras.

---
+++ Celestial_Corruption/Assets/CameraLockOnRotation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraLockOnRotation : MonoBehaviour
+{
+    public Transform player;
+    public Transform boss;
+    public CinemachineVirtualCamera lockOnCamera;
+    private CinemachineTransposer transposer;
+
+    void Start()
+    {
+        transposer = lockOnCamera.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    void Update()
+    {
+        AdjustCameraBasedOnPlayerPosition();
+    }
+
+    void AdjustCameraBasedOnPlayerPosition()
+    {
+        Vector3 playerToBoss = boss.position - player.position;
+        float angle = Vector3.SignedAngle(playerToBoss, transform.forward, Vector3.up);
+        playerToBoss.Normalize();
+        playerToBoss.y = 0;
+        playerToBoss = playerToBoss * 7;
+        playerToBoss.y += 1;
+        // Check if the player is on the opposite side of the boss relative to the camera
+        
+            // Force the camera to rotate 180 degrees by adjusting its FollowOffset
+            // This is a simple way to invert the camera's position. Adjust the values based on your game's scale and preferred distances
+        transposer.m_FollowOffset = new Vector3(-playerToBoss.x, playerToBoss.y, -playerToBoss.z);
+    }
+}
