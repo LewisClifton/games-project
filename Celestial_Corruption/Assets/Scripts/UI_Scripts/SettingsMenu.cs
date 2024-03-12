67e81274f59667d10ebaab3cc3329b6e5065afc6
MartinOravecSvK <oravec.martin01@gmail.com>
2024-03-12 18:55:57 +0000

    Added more functionality

--- Celestial_Corruption/Assets/Scripts/UI_Scripts/SettingsMenu.cs
+++ Celestial_Corruption/Assets/Scripts/UI_Scripts/SettingsMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
 using TMPro;
 
 public class SettingsMenu : MonoBehaviour
@@ -8,16 +10,18 @@ public class SettingsMenu : MonoBehaviour
 
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private Cinemachine.CinemachineFreeLook playerCamera;
-    [SerializeField] private GameObject sensitivitySliderX;
-    [SerializeField] private GameObject sensitivitySliderY;
+    [SerializeField] private Slider sensitivitySliderX;
+    [SerializeField] private Slider sensitivitySliderY;
 
-    private GameObject sliderX;
-    private GameObject sliderY;
+    private Slider sliderX;
+    private Slider sliderY;
     private TMP_InputField sliderValueX;
     private TMP_InputField sliderValueY;
 
     private float cameraSensitivityXaxis;
     private float cameraSensitivityYaxis;
+    private float cameraSensitivityConstantX;
+    private float cameraSensitivityConstantY;
 
     void Awake()
     {
@@ -31,6 +35,10 @@ public class SettingsMenu : MonoBehaviour
     {
         cameraSensitivityXaxis = playerCamera.m_XAxis.m_MaxSpeed;
         cameraSensitivityYaxis = playerCamera.m_YAxis.m_MaxSpeed;
+
+        cameraSensitivityConstantX = cameraSensitivityXaxis;
+        cameraSensitivityConstantY = cameraSensitivityYaxis;
+
         sliderValueX.text = cameraSensitivityXaxis.ToString();
         sliderValueY.text = cameraSensitivityYaxis.ToString();
 
@@ -41,15 +49,37 @@ public class SettingsMenu : MonoBehaviour
     public void SetCameraSensitivityX(float sensitivity)
     {
         Debug.Log("Camera sensitivity X set to: " + sensitivity);
-        playerCamera.m_XAxis.m_MaxSpeed = sensitivity;
-        sliderValueX.text = sensitivity.ToString();
+        playerCamera.m_XAxis.m_MaxSpeed = sensitivity * cameraSensitivityConstantX;
+        sliderValueX.text = playerCamera.m_XAxis.m_MaxSpeed.ToString();
     }
 
     public void SetCameraSensitivityY(float sensitivity)
     {
         Debug.Log("Camera sensitivity Y set to: " + sensitivity);
-        playerCamera.m_YAxis.m_MaxSpeed = sensitivity;
-        sliderValueY.text = sensitivity.ToString();
+        playerCamera.m_YAxis.m_MaxSpeed = sensitivity * cameraSensitivityConstantY;
+        sliderValueY.text = playerCamera.m_YAxis.m_MaxSpeed.ToString();
+    }
+
+    public void SetCameraSensitivityX(string sensitivity)
+    {
+        float value;
+        if (float.TryParse(sensitivity, out value))
+        {
+            playerCamera.m_XAxis.m_MaxSpeed = value;
+            sliderValueX.text = playerCamera.m_XAxis.m_MaxSpeed.ToString();
+            sliderX.value = value / cameraSensitivityConstantX;
+        }
+    }
+
+    public void SetCameraSensitivityY(string sensitivity)
+    {
+        float value;
+        if (float.TryParse(sensitivity, out value))
+        {
+            playerCamera.m_YAxis.m_MaxSpeed = value;
+            sliderValueY.text = playerCamera.m_YAxis.m_MaxSpeed.ToString();
+            sliderY.value = value / cameraSensitivityConstantY;
+        }
     }
 
     // public void SetSensitivityX(float sensitivity)
