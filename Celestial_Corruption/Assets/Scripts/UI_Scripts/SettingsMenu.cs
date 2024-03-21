437c06e1f16c62e402e693c947c09f98e9b2f296
MartinOravecSvK <oravec.martin01@gmail.com>
2024-03-21 15:41:13 +0000

    Update (bugs...)

--- Celestial_Corruption/Assets/Scripts/UI_Scripts/SettingsMenu.cs
+++ Celestial_Corruption/Assets/Scripts/UI_Scripts/SettingsMenu.cs
@@ -19,6 +19,7 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] private GameObject LeftArrow;
     [SerializeField] private GameObject RightArrow;
     [SerializeField] private GameObject[] settingsWindows;
+    [SerializeField] private GameObject[] settingsButtons;
     private int currentSettingsWindow = 0;
 
     void Awake()    
@@ -32,12 +33,17 @@ public class SettingsMenu : MonoBehaviour
         // Activates buttons
         LeftArrow.SetActive(true);
         RightArrow.SetActive(true);
+        for (int i = 0; i < settingsButtons.Length; i++)
+        {
+            settingsButtons[i].SetActive(true);
+        }
 
         SetSettingsWindow(0);
     }
 
     public void SetSettingsWindow(int window)
     {
+        Debug.Log("Setting window to " + window);
         settingsWindows[currentSettingsWindow].SetActive(false);
 
         settingsWindows[window].SetActive(true);
@@ -55,6 +61,20 @@ public class SettingsMenu : MonoBehaviour
     {
         settingsWindows[currentSettingsWindow].SetActive(false);
         currentSettingsWindow = (currentSettingsWindow - 1 + settingsWindows.Length) % settingsWindows.Length;
+        settingsWindows[currentSettingsWindow].SetActive(true);
+    }
+
+    public void ChangeToCamSensitivityMenu()
+    {
+        settingsWindows[currentSettingsWindow].SetActive(false);
+        currentSettingsWindow = 0;
+        settingsWindows[currentSettingsWindow].SetActive(true);
+    }
+
+    public void ChangeToKeybindsMenu()
+    {
+        settingsWindows[currentSettingsWindow].SetActive(false);
+        currentSettingsWindow = 1;
         settingsWindows[currentSettingsWindow].SetActive(true);
     }
 }
